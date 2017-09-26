ed1fa8cfbfd3878c320389cb65c93d83221d67d5
Lambert W <law@microsoft.com>
2017-09-26 14:46:33 -0700

    Update dist

--- src/reducers/pullout.ts
+++ src/reducers/pullout.ts
@@ -13,30 +13,12 @@ export interface IState {
     isVisible: boolean;
 }
 
-// export const reducer = combineReducers<IState>({
-//     isExpanded: (state = false, action) => {
-//         const {
-//             type,
-//             title,
-//         } = action;
-//         console.log('action');
-//         switch (type) {
-//             case TOGGLE_PULLOUT:
-//                 return {
-//                     title,
-//                 };
-//             default: return state;
-//         }
-//     },
-// });
-
 export const reducer = combineReducers<IState>({
     title: (state = 'Default title', action) => {
         const {
             type,
             title,
         } = action;
-        console.log('action');
         switch (type) {
             case TOGGLE_PULLOUT:
                 return title;
